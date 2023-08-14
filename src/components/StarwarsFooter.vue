a33c10aad9a0feb34ea4a4ee33f6e294b94a2ff0
essential14 <essential193@gmail.com>
2023-08-14 16:16:36 +0900

    [ADD] global component 사용

--- src/components/StarwarsFooter.vue
+++ src/components/StarwarsFooter.vue
@@ -1,5 +1,6 @@
 <template>
-  <h3>Copyright 2023 by essential193.</h3>
+  <h3>Copyright 2023 by essential193</h3>
+  <CustomButton>Click Me!</CustomButton>
 </template>
 <script>
 export default {};
